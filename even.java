3878a08c3164aa4a43f74ba3137eae0c2e3d11f2
maha1997 <mahalakshmi.s1997@gmail.com>
2017-06-12 22:46:39 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,15 @@
+public class even
+{
+public static void main(String[] args)
+{
+int n=57;
+System.out.println("printing even numbers between 23 to 57 are);
+for(int i=23;i<=n;i++)
+{
+if(i%2==0)
+{
+System.out.print(i+" ");
+}
+}
+}
+}
