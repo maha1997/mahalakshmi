b8dba7d8761a5619ceb66ada2422f969af025dc8
maha1997 <mahalakshmi.s1997@gmail.com>
2017-06-12 23:11:23 +0530

    Create palin.java

---
+++ palin.java
@@ -0,0 +1,18 @@
+public class palindrome
+{
+public static void main(String[] args)
+{
+int num=Integer.parseInt(args[0]);
+int n=num;
+int reverse=0,remainder;
+while(num>0){
+remainder=num%10;
+reverse=reverse*10+remainder;
+num=num/10;
+}
+if(reverse==n)
+System.out.println(n+"is a palindrome");
+else
+System.out.println(n+"is not a palindrome");
+}
+}
