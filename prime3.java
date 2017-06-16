5a7e6a05890f66cae24aaa6b7a5a017e1119d87d
maha1997 <mahalakshmi.s1997@gmail.com>
2017-06-16 18:14:22 +0530

    Create prime3.java

---
+++ prime3.java
@@ -0,0 +1,13 @@
+class CountPrimes
+
+{ 
+public static void main(String s[])
+{
+    {
+        System.out.println("The number of primes in the given range are : " + count Primes(5, 15));
+
+    }
+
+public static int count Primes(int rangeStart, int rangeEnd) 
+}
+} 
